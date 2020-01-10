db43cd804e9e662981ae21366c3aaabb3875bab2
Ivan Razgulin <ikrazgulin@yamoney.ru>
2020-01-10 10:55:01 +0300

    BACKEND-3184 Обновлён `yamoney-architecture-test-plugin`=`1.5.0`->`1.6.0`

--- src/main/java/ru/yandex/money/gradle/plugins/gradleproject/ExtensionConfigurator.java
+++ src/main/java/ru/yandex/money/gradle/plugins/gradleproject/ExtensionConfigurator.java
@@ -98,8 +98,6 @@ public class ExtensionConfigurator {
 
     private static void configureArchitectureTestPlugin(Project project) {
         ArchitectureTestExtension architectureTestExtension = project.getExtensions().getByType(ArchitectureTestExtension.class);
-        architectureTestExtension.getInclude().addAll(Arrays.asList(
-                "check_unique_enums_codes",
-                "check_unique_enums_secondary_codes"));
+        architectureTestExtension.getInclude().add("check_unique_enums_codes");
     }
 }
